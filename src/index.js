ec85040b573961ff72340f89fdaa5011d03254b0
pokolga <pokolga1801@gmail.com>
2022-01-25 23:46:48 +0300

    fix: mistake in order of command

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  if (str.length % 2 === 1) return false;
   const myArr = [];
   for (let i = 0; i < str.length; i++) {
     let symb = str[i];
-    bracketsConfig.forEach((item) => { if (item[0] === symb) myArr.push(item[1]) });
-    if (symb === myArr[myArr.length - 1]) myArr.pop();
+    if (symb === myArr[myArr.length - 1]) {
+      myArr.pop();
+    }
+    else {
+      bracketsConfig.forEach((item) => { if (item[0] === symb) { myArr.push(item[1]) } });
+    }
   }
   return (myArr.length === 0) ? true : false;
-
-
 }
