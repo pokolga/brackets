287505bb695275b5d691f7add80675507a9a1330
pokolga <pokolga1801@gmail.com>
2022-01-25 23:15:58 +0300

    init: bracket script

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  const myArr = [];
+  for (let i = 0; i < str.length; i++) {
+    let symb = str[i];
+    bracketsConfig.forEach((item) => { if (item[0] === symb) myArr.push(item[1]) });
+    if (symb === myArr[myArr.length - 1]) myArr.pop();
+  }
+  return (myArr.length === 0) ? true : false;
+
+
 }
